45bb37fe903794f1212aeb7bd57cfb798799df1e
Nam Nguyen <namhnguy@gmail.com>
2022-02-21 13:45:26 -0600

    finished

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+
+function createServer() {
+    const port = process.env.PORT || 8080
+
+    const server = http.createServer((req, res) => {
+        let q = url.parse(req.url, true);
+        let filename = '';
+        if (q.pathname === '/') {
+            filename = './index.html'
+        }
+        else if (q.pathname === '/about') {
+            filename = './about.html'
+        }
+        else if (q.pathname === '/contact-me') {
+            filename = './contact-me.html'
+        }
+        else {
+            filename = './404.html';
+        }
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'text/html');
+                return res.end('404 Not Found');
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            return res.end();
+        })
+    })
+    server.listen(port, () => {
+        console.log(`Server running at port ${port}`)
+    })
+}
+
+createServer();
+
+
+// let homeURL = new URL('localhost:8080', 'localhost:8080');
+// let aboutURL = new URL('localhost:8080/about', 'localhost:8080/about');
+// let contactURL = new URL('localhost:8080/contact-me', 'localhost:8080/contact-me');
+// console.log(myURL);
